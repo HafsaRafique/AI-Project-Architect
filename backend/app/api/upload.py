9cf807217b2c7e0547f07f8846f5798d91e2ac15
HafsaRafique <rafiquehafsa456@gmail.com>
2026-07-04 20:55:55 +0500

    initial commit

---
+++ backend/app/api/upload.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter, File, UploadFile
+from app.services.extractor import save_and_extract_zip
+router = APIRouter(
+    prefix = "/api",
+    tags = ["Upload"]
+)
+@router.post("upload/")
+async def upload_repository(file: UploadFile = File(...)):
+    return await save_and_extract_zip(file)
