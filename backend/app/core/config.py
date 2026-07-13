ecfabbf16a46da8fb9054f4cf04c698f55748ec3
HafsaRafique <139237723+HafsaRafique@users.noreply.github.com>
2026-07-13 21:55:19 +0500

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 HF_TOKEN = os.getenv("HF_TOKEN")
 
-QDRANT_HOST = os.getenv("QDRANT_HOST", "localhost")
-QDRANT_PORT = int(os.getenv("QDRANT_PORT", 6333))
+QDRANT_URL = os.getenv("QDRANT_URL")
+QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
 
 COLLECTION_NAME = os.getenv(
     "COLLECTION_NAME",
